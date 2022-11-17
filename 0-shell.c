e89d8314a0c34252e065354b8b0977ce1bbca0bc
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-11-17 11:12:52 -0800

    inicommit

--- 0-shell.c
+++ 0-shell.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 
 /**
- * SS_Read_Line: reads entire line from stream
+ * SS_Read_Line - read entire line
  *
  *Return:pointer to line read
  */
- char *SS_Read_Line(void)
- {
+char *SS_Read_Line(void)
+{
 	char *lnPtr = NULL;
 	size_t n = 0;
 
 	if (getline(&lnPtr, &n, stdin) == -1)
 	{
 		if (feof(stdin))
-		   exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		else
 		{	perror("readline");
 			exit(EXIT_FAILURE);
@@ -24,26 +24,25 @@
 	return (lnPtr);
 }
 /**
-*main - UNIX command line interpreter
+*main - check the code
 *@argc:number of argument
-@argv:argument
+*@argv:argument
 *
-Return: return 0
+*Return: return 0
 */
 int main(int argc, char **argv)
 {
 	char *line;
+
 	if (argc < 0)
 	{
 		fprintf(stderr, "Usage: %s <file>\n", *argv);
 		exit(EXIT_FAILURE);
 	}
-	do{
-		printf("$");
-		line =SS_Read_Line();
+	do {
+		printf("$ ");
+		line = SS_Read_Line();
 		printf(" %s", line);
 		free(line);
 	} while (1);
 }
-
-			
