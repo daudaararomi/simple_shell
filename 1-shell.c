390b12a4cc1b0f9b96ef824fcc54c8120191da1e
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-11-19 10:45:43 -0800

    1-shell.c

--- 1-shell.c
+++ 1-shell.c
@@ -1,54 +1,106 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include "main.h"
-/**
- * main - a UNIX command line interpreter
- *@ac: number of command
- *@argv:vector of command
- * Return: Nothing
- */
-int main(int ac, char **argv)
-{
-	char *lineptr = NULL;
-	size_t n = 0;
-	ssize_t nchars_read;
-	const char *delim = " \n";
-	char *token;
-	(void)ac;
 
-	while (1)
+/**
+ * _exec - to execute a command
+ *@args: command to execute
+ *
+ *Return: Nothing
+ */
+void _exec(char **args)
+{
+	int status;
+	pid_t child_pid = fork();
+
+	if (child_pid == 0)
 	{
-		printf("arafun $");
-		nchars_read = getline(&lineptr, &n, stdin);
-		if (nchars_read == -1)
+		execvp(args[0], args);
+		perror("error");
+		exit(1);
+	}
+	else if (child_pid > 0)
+	{
+		do {
+			waitpid(child_pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	else
+	{
+		perror("error");
+	}
+}
+
+/**
+ * _split_line - to split user commands to individual command for execution
+ *@line: pointer to line of commands
+ * Return: pointer splitted command
+ */
+char **_split_line(char *line)
+{
+	int length = 0, i = 0;
+
+	while (!line)
+	{
+		++line;
+		i++;
+	}
+	char **tokens = malloc(i * sizeof(char *));
+
+	char *delimiters = " \t\r\n";
+	char *token = strtok(line, delimiters);
+
+	if (!tokens)
+	{
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (token != NULL)
+	{
+		tokens[length] = token;
+		length++;
+		token = strtok(NULL, delimiters);
+	}
+
+	tokens[length] = NULL;
+	return (tokens);
+}
+
+/**
+ * _read_line - read in commands from standard input
+ * Return: pointer to commands read
+ */
+char *_read_line(void)
+{
+	char *line = NULL;
+	size_t buflen = 0;
+
+	if (getline(&line, &buflen, stdin) == -1)
+	{
+		if (feof(stdin))
 		{
-			if (feof(stdin))
-			{
-				exit(EXIT_SUCCESS);
-			}
-			else
-			{
-				perror("readline");
-				exit(EXIT_FAILURE);
-			}
-		}
-		argv = malloc(nchars_read * sizeof(char *));
-		if (!argv)
-		{
-			fprintf(stderr, "lsh: allocation error\n");
+			perror("readline");
 			exit(EXIT_FAILURE);
 		}
-		token = strtok(lineptr, delim);
-		while (token != NULL)
-		{
-			*argv = token;
-			token = strtok(NULL, delim);
-		}
-		argv[1] = NULL;
-		execmd(argv);
 	}
-	free(argv);
-	free(lineptr);
+	return (line);
+}
+
+/**
+ *main - simple shell
+ * Return: Nothing
+ */
+int main(void)
+{
+	while (true)
+	{
+		printf("arafun $  ");
+		char *line = _read_line();
+		char **tokens = _split_line(line);
+
+		if (tokens[0] != NULL)
+			_exec(tokens);
+
+		free(tokens);
+		free(line);
+	}
 }
--- main.h
+++ main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void _exec(char **args);
 void execmd(char **argv);
+char** _split_line(char *line);
+char* _read_line();
 char *SS_Read_Line(void);
 #endif /*MAIN_H */
